c28a1864adc6f309271794d8e778cf269274cee8
Nicole Law <nicole.law12@gmail.com>
2023-03-30 19:40:48 -0400

    URL Shortening Part1

--- express_server.js
+++ express_server.js
@@ -4,45 +4,69 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));//middleware
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
 
-app.get("/urls.json", (req, res) => {
-  res.send(urlDatabase);
-});
+// app.get("/urls.json", (req, res) => {
+//   res.send(urlDatabase);
+// });
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  const { longURL } = req.body;
+  const randomID = generateRandomStr();
+  urlDatabase[randomID] = longURL;
+  console.log(urlDatabase);
+  res.send("Ok");
+});
+
+const generateRandomStr = function() {
+  const alphaNum = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let result = "";
+  for (let i = 0; i < 6; i++) {
+    result += alphaNum.charAt(Math.floor(Math.random() * alphaNum.length));
+  }
+  return result;
+};
+
 // app.get("/urls/:id", (req, res) => {
 //   res.send(req.params);
 // });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase.id};
+  const templateVars = { id: req.params.id, longURL: urlDatabase.id };
   res.render("urls_show", templateVars);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World<b></body></html>\n");
-});
 
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World<b></body></html>\n");
+// });
 
-app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
-});
+// app.get("/set", (req, res) => {
+//   const a = 1;
+//   res.send(`a = ${a}`);
+// });
+
+// app.get("/fetch", (req, res) => {
+//   res.send(`a = ${a}`);
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
